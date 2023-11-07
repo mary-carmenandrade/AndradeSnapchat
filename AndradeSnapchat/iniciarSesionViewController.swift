fd2a8b92beb49f06c492f00e4e540e5895858108
Andrade_Mary <mary.andrade@tecsup.edu.pe>
2023-11-07 18:34:20 -0500

    el mas nuevo

--- AndradeSnapchat/iniciarSesionViewController.swift
+++ AndradeSnapchat/iniciarSesionViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
@@ -18,6 +20,32 @@ class iniciarSesionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func iniciargoogle(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+                        let config = GIDConfiguration(clientID: clientID)
+                        GIDSignIn.sharedInstance.configuration = config
+
+                        // Inicia el flujo de inicio de sesión con Google
+                        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+                            if let error = error {
+                                print("Error al iniciar sesión con Google: \(error.localizedDescription)")
+                            } else if let user = result?.user,
+                                      let idToken = user.idToken?.tokenString {
+                                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+                                // Inicia sesión en Firebase con las credenciales de Google
+                                Auth.auth().signIn(with: credential) { _, firebaseError in
+                                    if let firebaseError = firebaseError {
+                                        print("Error al iniciar sesión en Firebase: \(firebaseError.localizedDescription)")
+                                    } else {
+                                        print("Inicio de sesión en Firebase exitoso")
+                                        // Realiza cualquier acción adicional que necesites después del inicio de sesión.
+                                    }
+                                }
+                            }
+                        }
+        
+    }
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailTextField.text!, password:passwordTextField.text!) {(user, error) in print("Intentando Iniciar Sesion")
             if error != nil{
